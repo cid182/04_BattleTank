e4a47e174c78d5379d0347672264ba1189af05d4
cid182 <supermonkeymail182@yahoo.com>
2017-11-12 13:21:19 -0500

    Section 4, lecture 122, post-video commit... About to make a Low-poly version of main map (wish me luck)

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "Tank.h"
+
+
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("TankPlayerController Begin Play"));
+	GetControlledTank();
+
+	auto MyControlledTank = GetControlledTank();
+
+	if (!MyControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerControlller IS NOT possessing a tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerControlller is possessing %s"), *(MyControlledTank->GetName()));
+	}
+}
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
